83c6503c76408200b22722f2b3aa98d8cdc0f7c9
Mokashaa <mohamedokasha125@gmail.com>
2018-12-21 15:01:58 +0200

    update

--- Device.v
+++ Device.v
@@ -1,8 +1,8 @@
-module Device (DeviceAddress[1:0],force_Request,address_To_Contact[1:0],WriteData,RW,GNT,REQ,AD[31:0],IRDY,TRDY,FRAME,CBE[3:0],DEVSEL,CLK,RST);
+module Device (DeviceAddress[1:0],force_Request,address_To_Contact[1:0],WriteData,WR,GNT,REQ,AD[31:0],IRDY,TRDY,FRAME,CBE[3:0],DEVSEL,CLK,RST);
 
 output reg REQ;
 
-input  RW,CLK,GNT,RST,force_Request;
+input  WR,CLK,GNT,RST,force_Request;
 input  [1:0] address_To_Contact;
 input  [1:0] DeviceAddress;
 input  [31:0] WriteData;
@@ -19,22 +19,23 @@ reg [31:0] REG_D;
 reg [1:0]  REG_A;
 reg [3:0]  REG_CBE;
 reg [31:0] Memory [0:9];
-reg [31:0] Data2write;
+//reg [31:0] Data2write;
 reg MasterNotSlave;
 integer    counter;
 reg SelectedAddress; 
-reg par;
+reg [2:0] state;
 reg [2:0] countREQ;
+integer i;
 
 
-
+assign REQ = force_Request;
 assign DEVSEL = SelectedAddress? 1'b0 : 1'bz ;
-
+/*
 always @ (posedge force_Request)
 begin
 REQ<=0;
 end
-
+*/
 always @ (negedge GNT)
 begin
 MasterNotSlave<=1'b1;
@@ -45,44 +46,45 @@ always @(posedge CLK,RST)
 begin
 if(RST)
 begin 
-par=0;
+state=0;
 counter =0;
-countREQ =0;
+countREQ <=0;
 MasterNotSlave=0;
 end 
 
 else 
 begin
-    if(~force_Request) begin counter = counter + 1; end
+    if(~force_Request) begin countREQ = countREQ + 1; end
 	
 	if(MasterNotSlave) /* Master Scope */
 	begin
-		if(RW) /* Write from master side */
+		if(WR) /* Write from master side */
 		begin
-			case (par)
+			case (state)
 			0: @(negedge CLK)
 				   begin
 				   REG_FRAME <= 1'b0;
 				   REG_A <= address_To_Contact;
-				   REG_CBE <= RW;//command el write
-				   par = 1;
+				   REG_CBE <= WR;//command el write
+				   state = 1;
 				   end
 			   
 			1: begin
 				   @(negedge CLK)
 				   begin
-				   if(counter > 1)
+				   if(countREQ > 1)
 				   begin
 				   REG_IRDY <= 1'b0;
 				   REG_CBE <= 4'b1111;
 				   REG_D <= WriteData;
-				   counter = counter - 1;
+				   countREQ = countREQ - 1;
 				   end
-				   else if (counter == 1)
+				   else if (countREQ == 1)
 				   begin 
-				   par = 2; 
+				   state = 2; 
 				   REG_FRAME <= 1'b1; 
 				   REG_D <= WriteData;
+				   countREQ = countREQ - 1;
 				   end
 				   end 
 			   end
@@ -91,7 +93,7 @@ begin
 				   begin
 				   REG_IRDY <= 1'b1;
 				   REG_D <= 32'bzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz;
-				   par=0;
+				   state=0;
 				   MasterNotSlave<=1'b0;
 				   end
                end			   
@@ -100,7 +102,7 @@ begin
 		
 		else /* Read from master side */
 		begin
-			case (par)
+			case (state)
 
 				3'b000: begin 
 							@(negedge CLK)
@@ -108,7 +110,7 @@ begin
 							REG_FRAME<=1'b0;
 							REG_A<=address_To_Contact;
 							REG_CBE <= WR; //command el write
-							par=1;
+							state=1;
 							end
 					    end		
 				
@@ -117,34 +119,35 @@ begin
 							begin
 							REG_A<=2'bzz;
 							REG_IRDY<=1'b0;
-							par=2;
+							state=2;
 							end
 						end	
 
 				3'b010: begin 
-							par=3;
+							state=3;
 						end	
 
 				3'b011:  begin
-					  if(~DEVSEL&&~TRDY&&~IRDY&&(countREQ>0))
+					  if(~DEVSEL&&~TRDY&&(countREQ>0))
 					      begin
-							countREQ --;
+							countREQ =countREQ - 1;
 							Memory[counter]=AD;
 							if(counter ==9)begin counter =0; end
-							else begin counter ++; end
+							else begin counter = counter + 1; end
 
-							if (counterREQ ==1 )
+							if (countREQ ==1 )
 							begin
 							@(negedge CLK)
 							begin FRAME=1; end
 							end
 
-							else if (counterREQ==0)
+							else if (countREQ==0)
 							begin
 							@(negedge CLK)
 							begin
 								REG_IRDY=1;
-								readMasterState=0;
+								state=0;
+								MasterNotSlave<=1'b0;
 							end
 							end
 						 end
@@ -155,10 +158,10 @@ begin
 	
 	else	/* Slave scope */
 	begin
-		case (par)
+		case (state)
 			0: if (adress == adres 3al bus )
 				begin
-					//increment par by 2 in each state
+					//increment state by 2 in each state
 					//odd numbers for write
 					//even numbers for read
 					if(CBE == ) /* Write from slave side */
@@ -168,7 +171,7 @@ begin
 						begin
 						REG_DEVSEL<=1'b0;
 						REG_TRDY<=1'b0;
-						par=1;
+						state=1;
 						end //end of negative edge	
 					end
 					else if (CBE == ) /* read from slave side*/
@@ -177,12 +180,12 @@ begin
 						@(negedge CLK)
 						begin
 						REG_DEVSEL<=1'b0;
-						par=2;
+						state=2;
 						end //end of negative edge	
 
 					end
 				end	
-			1: for(integer i=0;i<4;i=i+1) //for loop that checks BE and assigns the corresponding bits into the memory
+			1: for(i=0;i<4;i=i+1) //for loop that checks BE and assigns the corresponding bits into the memory
 				begin                      
 				  if(CBE[i]==1'b1)
 					 begin 
@@ -196,7 +199,7 @@ begin
 					begin
 					REG_DEVSEL<=1'b1;
 					REG_TRDY<=1'b1;
-					par=0;
+					state=0;
 					end
 				end	
 
@@ -204,7 +207,7 @@ begin
 				begin
 				REG_TRDY<=1'b0;
 				REG_D<=(IRDY)?Data2write;
-				par=3;
+				state=3;
 				end
 
 			3: @(negedge CLK)
@@ -215,10 +218,10 @@ begin
 					end
 					else
 					begin
-					REG_D<=(IRDY)?Data2write;
+					//REG_D<=(IRDY)?Data2write;
 					REG_DEVSEL<=1'b1;
 					REG_TRDY<=1'b1;
-					par=0;
+					state=0;
 					end
 				end
 			endcase
